4e46d78c6dc30e5fb19ed8bf8422d7eaeece83f1
Kindrat22 <volodimirkindrat22@gmail.com>
2025-06-22 16:02:06 +0300

    Added PDF generation with WeasyPrint

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.cv_list, name='cv_list'),
     path('cv/<int:pk>/', views.cv_detail, name='cv_detail'),
+    path('cv/<int:pk>/pdf/', views.cv_pdf, name='cv_pdf'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 from .models import CV
+import weasyprint
 
 
 def cv_list(request):
@@ -10,3 +13,13 @@ def cv_list(request):
 def cv_detail(request, pk):
     cv = get_object_or_404(CV, pk=pk)
     return render(request, 'main/cv_detail.html', {'cv': cv})
+
+
+def cv_pdf(request, pk):
+    cv = get_object_or_404(CV, pk=pk)
+    html = render_to_string('main/cv_pdf.html', {'cv': cv})
+    pdf = weasyprint.HTML(string=html).write_pdf()
+    filename = f"cv_{cv.firstname}_{cv.lastname}.pdf"
+    response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="{filename}"'
+    return response
